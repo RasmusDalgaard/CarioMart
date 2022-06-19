10ed9b10fa7fbfcc22c072f76dec75289127b73d
Janussr <Janusstivang@gmail.com>
2022-06-19 15:43:15 +0200

    Add barrel obstacle

---
+++ Assets/Scripts/StrafingMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrafingMovement : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+
+    private int current = 0;
+
+
+
+    void Update()
+    {
+
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length;
+
+    }
+}
---
+++ Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour
+{
+
+
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotspeed;
+    public float speed;
+    float WPradius = 1;
+
+    void Update()
+    {
+        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
