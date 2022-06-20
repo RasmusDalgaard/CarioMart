c5d1ccac7a564f5785a0bdb433c1dd81afba6f57
FrederikBA <frederikbandersen98@gmail.com>
2022-06-20 11:16:48 +0200

    start lobby ui

---
+++ Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameScript : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        Debug.Log("Start");
+    }
+
+    public void LeaveGame()
+    {
+        Debug.Log("Leave");
+    }
+
+}
