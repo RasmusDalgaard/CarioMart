741d6a365dc0325c48ada1851acb9f84ee90e23f
FrederikBA <frederikbandersen98@gmail.com>
2022-06-20 16:15:31 +0200

    conditional rendering on start game if host or not

--- Assets/Scripts/PUN2_StartLobby.cs
+++ Assets/Scripts/PUN2_StartLobby.cs
@@ -10,28 +10,51 @@ public class PUN2_StartLobby : MonoBehaviourPunCallbacks
     {
         //Styling
 
-        GUIStyle myButtonStyle = new GUIStyle(GUI.skin.button);
-        myButtonStyle.fontSize = 25;
+        //Start button styling
+        GUIStyle startButtonStyle = new GUIStyle(GUI.skin.button);
+        startButtonStyle.fontSize = 25;
+        Texture2D result = new Texture2D(1, 1);
 
-        GUIStyle myStyle = new GUIStyle();
-        myStyle.fontSize = 30;
-        myStyle.normal.textColor = Color.white;
+        //Create transparent background for buttons when player != host
+        Color[] pix = new Color[] { Color.clear };
+        result.SetPixels(pix);
+        result.Apply();
+        if(!PhotonNetwork.IsMasterClient) { 
+            startButtonStyle.normal.background = result;
+            startButtonStyle.hover.background = result;
+            startButtonStyle.active.background = result;
+            startButtonStyle.fontSize = 35;
+        }
+
+        //Leave button styling
+        GUIStyle leaveButtonStyle = new GUIStyle(GUI.skin.button);
+        leaveButtonStyle.fontSize = 25;
+
+        //Text & Label styling
+        GUIStyle textStyle = new GUIStyle();
+        textStyle.fontSize = 30;
+        textStyle.normal.textColor = Color.white;
 
 
         if (PhotonNetwork.CurrentRoom == null)
             return;
 
         //Leave this Room
-        if (GUI.Button(new Rect(5, 5, 200, 50), "Leave Room", myButtonStyle))
+        if (GUI.Button(new Rect(5, 5, 200, 50), "Leave Room", leaveButtonStyle))
         {
             PhotonNetwork.LeaveRoom();
         }
 
+        //Check if player is host
+        string isHost = (PhotonNetwork.IsMasterClient ? "Start Game" : "Waiting for host to start");
+
         //Start Game
-        if  (GUI.Button(new Rect(Screen.width / 2-125, Screen.height / 2, 250, 75), "Start Game", myButtonStyle))
+        if  (GUI.Button(new Rect(Screen.width / 2-200, Screen.height / 2, 400, 75), isHost, startButtonStyle))
         {
-            Debug.Log("Start");
+            if(PhotonNetwork.IsMasterClient)
+            { 
             PhotonNetwork.LoadLevel("Playground");
+            } 
         }
 
         //Show the list of the players connected to this Room
@@ -39,7 +62,7 @@ public class PUN2_StartLobby : MonoBehaviourPunCallbacks
         {
             //Show if this player is a Master Client. There can only be one Master Client per Room so use this to define the authoritative logic etc.)
             string isMasterClient = (PhotonNetwork.PlayerList[i].IsMasterClient ? ": Host" : "");
-            GUI.Label(new Rect(5, 70+30 * i, 400, 50), PhotonNetwork.PlayerList[i].NickName + isMasterClient, myStyle);
+            GUI.Label(new Rect(5, 70+30 * i, 400, 50), PhotonNetwork.PlayerList[i].NickName + isMasterClient, textStyle);
         }
     }
 
