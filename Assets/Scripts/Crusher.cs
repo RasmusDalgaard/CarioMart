00535baa980beb5ee4e5cfb0ca7a9b25901ef409
Janussr <Janusstivang@gmail.com>
2022-06-22 20:09:00 +0200

    refactor crusher

--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -12,8 +12,8 @@ public class Crusher : MonoBehaviour
     void Start()
     {
         myAnim = GetComponent<Animator>();
-        myAnim.SetFloat("WaitTime", 1 / waitTime);
-        myAnim.Play("WaitTime", -1, animationOffset);
+        myAnim.SetFloat("WaitTime", 1 / waitTime);  //  1 / waitime, allows us to add time in seconds we want this animation to be.
+        myAnim.Play("WaitTime", -1, animationOffset);       //If we have multiple crushers in the scene, they will not be sync with eachother.
     }
 
 }
