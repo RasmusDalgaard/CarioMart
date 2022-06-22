d808e2c07eb4618f35354c10766626a7b2e1d36b
Janussr <Janusstivang@gmail.com>
2022-06-22 19:36:13 +0200

    refactor

--- Assets/Scripts/WaypointScript.cs
+++ Assets/Scripts/WaypointScript.cs
@@ -7,18 +7,19 @@ public class WaypointScript : MonoBehaviour
     public GameObject[] waypoints;
     int current = 0;
     public float speed;
-    float WPradius = 1;  //WaypointRadius
+    float WPradius = 1;  //WaypointRadius - if the speed or size of object too large, it might miss the waypoint. if the wpraidus is 1 it means it reached and is ready to go to next waypoint
 
     void Update()
     {
-        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius) //check if current position is less than Waypoint radis.
         {
-            current++;
-            if (current >= waypoints.Length)
+            current++;                                                                              //adds 1 to current, and makes us go to the next waypoint
+            if (current >= waypoints.Length)                                                        // if the current is >= waypoints length we have reached, and it sets current back to 0, so it keeps going in a loop
             {
                 current = 0;
             }
         }
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+        //moves the object between current and target. with Time.deltaTime * speed, we can make it go faster
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);    
     }
 }
